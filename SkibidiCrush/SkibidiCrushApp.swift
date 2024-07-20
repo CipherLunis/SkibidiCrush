4ad69974d9db707f297d025fd9beecbf5d995f36
Cipher Lunis <cipheryxinquiries@gmail.com>
2024-07-19 21:34:55 -0500

    Initial Commit

---
+++ SkibidiCrush/SkibidiCrushApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkibidiCrushApp.swift
+//  SkibidiCrush
+//
+//  Created by Cipher Lunis on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SkibidiCrushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
